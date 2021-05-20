528a60c89373f7a2fbfbfc29682d5ac10d448848
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-05-20 22:00:02 +0430

    solve problem with *shoma* position in orderList

--- resources/views/layouts/orderList.blade.php
+++ resources/views/layouts/orderList.blade.php
@@ -1,18 +1,13 @@
 <ul class="w3-ul ">
   @for($i=1;$i<9;$i++)
         <li class=" w3-row  " style=";height:60px">
-            <div class="w3-col l2  "> <span id="pn{{$i}}" class="w3-badge w3-left w3-white
-        w3-border  w3-right  w3-large " style="margin-top:8px ">{{$i}}</span>
-                <span id="pp{{$i}}" class='persian w3-hide' style='position: relative;top:-27px;right: 10px'>شما</span>
-                <span id="pps{{$i}}" class="" style="
-@if($i==4)
-                    position:relative;right: 0px;top:-32px
-                @else
-                    position:relative;right: -21px;bottom:-15px
-                @endif
-"></span>
+            <div class="w3-col l3  "><div  class="
+        " style="margin-top: 7px"> <div id="pn{{$i}}" class="w3-badge w3-white w3-border w3-large w3-right"  style="">{{$i}}</div>
+                    <span id="pp{{$i}}" class='persian w3-hide w3-right  w3-right-align ' style='position: relative;top:3px;' >شما</span></div>
+
+
             </div>
-            <div id="p{{$i}}" class="w3-col l10 w3-gray w3-padding w3-round-xxlarge  w3-center
+            <div id="p{{$i}}" class="w3-col l9 w3-gray w3-padding w3-round-xxlarge  w3-center
 "></div>
 
         </li>
