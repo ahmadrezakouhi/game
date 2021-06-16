7b73594a482686a56fce4e5fe49cd5c5eac83e85
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-06-16 07:29:32 +0430

    prevent user to enter alphabet in input

--- resources/views/game2/game.blade.php
+++ resources/views/game2/game.blade.php
@@ -412,6 +412,15 @@
             }
 
 
+
+           $('input').keypress(function (event) {
+            if (event.which < 48 || event.which > 57)
+            {
+                 event.preventDefault();
+            }
+           })
+
+
         })
     </script>
 
