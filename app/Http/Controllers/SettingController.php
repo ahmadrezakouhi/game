7f1d00168de2262cf870846058500c22b87f97f5
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-09-21 18:47:04 +0430

    edit update method

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -11,15 +11,19 @@ class SettingController extends Controller
     public function edit()
     {
 
-        $setting = Setting::all();
-        return view('settings.setting',compact('setting'));
+        $setting = Setting::first();
+        return view('settings.setting', compact('setting'));
     }
 
-    public function update(Request $request){
-
-        Setting::first()->update($request->all());
-        $request->session()->flash('setting','تنظیمات به روز رسانی شد.');
+    public function update(Request $request)
+    {
+        $setting = Setting::first();
+        if ($setting) {
+            Setting::first()->update($request->all());
+        } else {
+            Setting::create($request->all());
+        }
+        $request->session()->flash('settings', 'تنظیمات به روز رسانی شد.');
         return redirect('settings');
-
     }
 }
