b706697e3a2c228bd1338d4942737aac6a54ee37
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-09-21 17:17:41 +0430

    craete edit() in SettingController

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-  
 
+    public function edit()
+    {
+
+        $setting = Setting::all();
+        return view('settings.setting',compact('setting'));
+    }
+
+   
 }
