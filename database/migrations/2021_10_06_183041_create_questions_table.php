9a10fc93eba27aa12afbbaa92e4232e3fbd21701
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-06 22:10:29 +0330

    alter nullable on user_id column in questions table

--- database/migrations/2021_10_06_183041_create_questions_table.php
+++ database/migrations/2021_10_06_183041_create_questions_table.php
@@ -15,7 +15,7 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('user_id');
+            $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('result')->nullable();
             $table->integer('time')->nullable();
