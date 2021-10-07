b5aa0a9b39c7e0c554640c085a62ae5b431d35fe
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-07 06:52:59 +0330

    create questions method and format code

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -7,6 +7,7 @@ use App\User;
 use App\Rank;
 use App\Answer;
 use App\Money;
+use App\Question;
 use Carbon\Carbon;
 
 class AnswerController extends Controller
@@ -16,29 +17,31 @@ class AnswerController extends Controller
 
 
 
-    public function store_letter(Request $request){
+    public function store_letter(Request $request)
+    {
         $user = auth()->user();
         $user->update([
-           "letter"=>$request->get('letter'),
-           "letter_time"=>$request->get('letter_time'),
-          // "resolution"=>$request->get('resolution'),
+            "letter" => $request->get('letter'),
+            "letter_time" => $request->get('letter_time'),
+            // "resolution"=>$request->get('resolution'),
 
         ]);
-       return response()->json();
-
+        return response()->json();
     }
 
-    public function store_answer_question_game1(Request $request){
-        $input= $request->all();
-        $input['user_id']=auth()->user()->id;
+    public function store_answer_question_game1(Request $request)
+    {
+        $input = $request->all();
+        $input['user_id'] = auth()->user()->id;
         Answer::create($input);
         return response()->json();
     }
 
-    public function rank(Request $request){
-       $input = $request->all();
-       $input['user_id']= auth()->user()->id;
-       Rank::create($input);
+    public function rank(Request $request)
+    {
+        $input = $request->all();
+        $input['user_id'] = auth()->user()->id;
+        Rank::create($input);
 
         return response()->json($input);
     }
@@ -46,9 +49,19 @@ class AnswerController extends Controller
     public function money(Request $request)
     {
         $input = $request->all();
-        $input['user_id']=auth()->user()->id;
+        $input['user_id'] = auth()->user()->id;
         Money::create($input);
         return response()->json();
     }
+
+
+    public function questions(Request $request)
+    {
+        $input = $request->all();
+        $input['user_id']=auth()->user()->id;
+        Question::create($input);
+        return response()->json();
+    }
+
 
 }
