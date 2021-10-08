b5e208dc1c6cd9c03b01a8e73ef2e143f99376b4
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-08 21:30:57 +0330

    create estimate function in EstimateController

--- app/Http/Controllers/EstimateController.php
+++ app/Http/Controllers/EstimateController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Estimate;
 use Illuminate\Http\Request;
 
 class EstimateController extends Controller
 {
-    //
+    public function estimate(Request $request)
+    {
+        $input = $request->all();
+        $input['user_id']=auth()->user()->id;
+        Estimate::create($input);
+        return response()->json();
+    }
 }
