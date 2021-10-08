7197ef68904b9d91f3406f1221e12dcf7925f053
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-08 21:31:22 +0330

    create score function in ScoreController

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Score;
 use Illuminate\Http\Request;
 
 class ScoreController extends Controller
 {
-    //
+    public function score(Request $request)
+    {
+        $input = $request->all();
+        $input['user_id']=auth()->user()->id;
+        Score::crete($input);
+        return response()->json();
+    }
 }
